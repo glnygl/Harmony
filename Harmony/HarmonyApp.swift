ac2242db7b1bb65da479792244bf7e1270997cf5
Glny Gl <glnygl94@gmail.com>
2025-02-07 22:19:05 +0100

    :tada: Initial commit

---
+++ Harmony/HarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyApp.swift
+//  Harmony
+//
+//  Created by Glny Gl on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct HarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
