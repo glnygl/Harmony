60d80bff05ae69a79953e7907196a52e557f969c
Glny Gl <glnygl94@gmail.com>
2025-02-14 00:48:24 +0100

    Convert scroll view from list for pagination

--- Harmony/Features/Favorites/FavoritesView.swift
+++ Harmony/Features/Favorites/FavoritesView.swift
@@ -12,27 +12,28 @@ struct FavoritesView: View {
 
   @Bindable var store: StoreOf<FavoritesFeature>
 
-    var body: some View {
-      NavigationStack {
-        if store.trackList.isEmpty {
-          VStack(spacing: 8) {
-            Image("music")
-              .resizable()
-              .frame(width: 80, height: 80)
-            VStack(spacing: 4) {
-              Text("No Favorites")
-                .font(.system(size: 18, weight: .semibold))
-              Text("You can add tracks to your favorites by clicking heart button.")
-                .font(.system(size: 16))
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.gray)
-            }
+  var body: some View {
+    NavigationStack {
+      if store.trackList.isEmpty {
+        VStack(spacing: 8) {
+          Image("music")
+            .resizable()
+            .frame(width: 80, height: 80)
+          VStack(spacing: 4) {
+            Text("No Favorites")
+              .font(.system(size: 18, weight: .semibold))
+            Text("You can add tracks to your favorites by clicking heart button.")
+              .font(.system(size: 16))
+              .multilineTextAlignment(.center)
+              .foregroundStyle(.gray)
           }
-          .padding(.horizontal, 20)
-          .navigationTitle("Favorites")
-          .navigationBarTitleDisplayMode(.inline)
-        } else {
-          List {
+        }
+        .padding(.horizontal, 20)
+        .navigationTitle("Favorites")
+        .navigationBarTitleDisplayMode(.inline)
+      } else {
+        ScrollView {
+          LazyVStack {
             ForEach(store.trackList, id:\.id) { track in
               VStack(alignment: .leading) {
                 Button(action: {
@@ -43,12 +44,8 @@ struct FavoritesView: View {
                 .buttonStyle(PlainButtonStyle())
                 Divider()
               }
-              .listRowInsets(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 16))
-              .listRowSeparator(.hidden)
             }
           }
-          .listRowSpacing(0)
-          .listStyle(.plain)
           .scrollIndicators(.hidden)
           .fullScreenCover(
             item: $store.scope(state: \.trackDetailState, action: \.showTrackDetail)
@@ -60,17 +57,19 @@ struct FavoritesView: View {
           .navigationTitle("Favorites")
           .navigationBarTitleDisplayMode(.inline)
         }
-      }
-      .onAppear {
-        store.send(.fetchFavorites)
+        .padding(.horizontal)
       }
     }
+    .onAppear {
+      store.send(.fetchFavorites)
+    }
+  }
 }
 
 #Preview {
   FavoritesView(
     store: Store(initialState: FavoritesFeature.State()) {
       FavoritesFeature()
-     }
-   )
+    }
+  )
 }
--- Harmony/Features/TrackList/TrackListView.swift
+++ Harmony/Features/TrackList/TrackListView.swift
@@ -18,51 +18,50 @@ struct TrackListView: View {
       VStack {
         if store.searchText.isEmpty && !isSearchFocused {
           ScrollView {
-              VStack(alignment: .leading, spacing: 20) {
-                  PopularGenreView(store: store.scope(state: \.popularGenresState, action: \.popularGenresAction))
-                  PopularArtistsView(store: store.scope(state: \.popularArtistsState, action: \.popularArtistsAction))
+            VStack(alignment: .leading, spacing: 20) {
+              PopularGenreView(store: store.scope(state: \.popularGenresState, action: \.popularGenresAction))
+              PopularArtistsView(store: store.scope(state: \.popularArtistsState, action: \.popularArtistsAction))
 
-                VStack(alignment: .leading, spacing: 10) {
-                  Text("Grammy Winners")
-                    .font(.system(size: 18))
-                    .fontWeight(.bold)
-                    .padding(.horizontal)
-                    .foregroundStyle(.black).opacity(0.6)
+              VStack(alignment: .leading, spacing: 10) {
+                Text("Grammy Winners")
+                  .font(.system(size: 18))
+                  .fontWeight(.bold)
+                  .padding(.horizontal)
+                  .foregroundStyle(.black).opacity(0.6)
 
-                      ForEach(store.grammyTrackList, id: \.id) { track in
-                        VStack(alignment: .leading) {
-                          TrackListRowView(track: track)
-                            .onTapGesture {
-                              store.send(.listRowSelected(track))
-                            }
-                          Divider()
-                        }
-                        .padding(.horizontal, 20)
+                ForEach(store.grammyTrackList, id: \.id) { track in
+                  VStack(alignment: .leading) {
+                    TrackListRowView(track: track)
+                      .onTapGesture {
+                        store.send(.listRowSelected(track))
                       }
+                    Divider()
                   }
-                .padding(.top, -16)
+                  .padding(.horizontal, 20)
+                }
               }
-              .padding(.top, 4)
-              .padding(.bottom, 10)
+              .padding(.top, -16)
+            }
+            .padding(.top, 4)
+            .padding(.bottom, 10)
           }
           .scrollIndicators(.hidden)
         } else {
-          List {
-            ForEach(store.trackList, id:\.id) { track in
-              VStack(alignment: .leading) {
-                TrackListRowView(track: track)
-                  .onTapGesture {
-                    store.send(.listRowSelected(track))
-                  }
-                Divider()
+          ScrollView {
+            LazyVStack {
+              ForEach(store.trackList, id:\.id) { track in
+                VStack(alignment: .leading) {
+                  TrackListRowView(track: track)
+                    .onTapGesture {
+                      store.send(.listRowSelected(track))
+                    }
+                  Divider()
+                }
               }
-              .listRowInsets(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 16))
-              .listRowSeparator(.hidden)
             }
           }
-          .listRowSpacing(0)
-          .listStyle(.plain)
           .scrollIndicators(.hidden)
+          .padding(.horizontal)
         }
       }
       .searchable(text: $store.searchText, prompt: "What do you want to listen to?")
